7ce9181b63a82587e674adad6ad6f7327ff8f7a1
Sergio <serxa92@hotmail.com>
2025-04-03 12:23:24 +0200

    fix: corregido formato del botón en el componente Button

--- my-portfolio/components/Button/index.js
+++ my-portfolio/components/Button/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export const Button = (icon, text,url) => `
 <a href = "${url}"><button class="my-btn">
 <img src=${icon} alt='${text} icon'/>
-<h4>${text}</h4>
+<h4>${text}</h4> 
 </button></a>
 `;
 
